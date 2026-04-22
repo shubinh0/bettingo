ad983eca50b86c96c08cb734321782295f2e32e9
shubinh0 <egorshubin2000@gmail.com>
2026-04-22 16:59:00 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,5 @@ cd ~/www/bot
 
 # Создаем файл с токенами
 cat > config.py << EOF
-VK_TOKEN = vk1.a.2A_kXCYEE0IOBnwXIIt1w0GxuFZODotUAHGWR2OadsEVOaqN4pZJzEbg9W2xK08aR5z1oczMOzCr3ZCfCuwULpnB0Yn55pXl7FJxetvGpHTylL0__rdjMcwY0goSs3xE0j4Uxi6b0GNzCcfroHbWj-0OeyWfEb-MUwpuVZu0wYXM1_TIx_sUr9Lv-OPH61dJptim8YKgDDv9ljQq2BZ3QA
-RAPIDAPI_KEY = "скопированный_rapidapi_ключ"
-ODDS_API_KEY = ""
+
 EOF
